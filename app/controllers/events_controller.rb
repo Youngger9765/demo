b0e212b13bd7a73e36760c8a5a309855551cc273
purpleice9765 <purpleice9765@msn.com>
2015-09-29 22:33:23 +0800

    user_name add

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
 
+  before_action :authenticate_user!, :except => [:index]
+
   before_action :set_event, :only => [ :show, :edit, :update, :destroy]
 
   #get events/index
@@ -26,6 +28,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     
+    @event.user = current_user
     
 
     if @event.save
