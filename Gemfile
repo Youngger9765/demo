84713167e06cc674e752b1ab6da1d03cbf40cee1
purpleice9765 <purpleice9765@msn.com>
2015-09-29 16:42:36 +0800

    實作8

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'devise'
+
 gem "kaminari"
 
 gem 'bootstrap-sass', '~> 3.3.5'
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
 
   end
 
+
+  namespace :admin do
+    resources :events
+  end
+
   resources :people
 	get "welcome/say_hello" => "welcome#say"
 	get "welcome" => "welcome#index"
